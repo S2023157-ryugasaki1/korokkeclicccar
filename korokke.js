25cf674f019e98f22af983ae0816ceecac8eebde
S2023157-ryugasaki1 <155863561+S2023157-ryugasaki1@users.noreply.github.com>
2024-03-19 09:30:21 +0900

    Update korokke.js

--- korokke.js
+++ korokke.js
@@ -76,7 +76,7 @@
         }
 
         function rebyu(){
-            const url = "https://docs.google.com/forms/d/1oMk2sIXqeH8OrbbV_tCj48sBRepM5exJWsrouec4XFQ/prefill"
+            const url = "https://chat.google.com/dm/hzCi1gAAAAE/1Ujou0ZSF1s/1Ujou0ZSF1s?cls=10"
             window.open(url, '_blank')
         }
 
